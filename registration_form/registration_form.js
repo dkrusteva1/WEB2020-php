4a9aec5d25c91f9b3cc3bb53c10489ab61e5852b
dkrusteva1 <dessynceto@abv.bg>
2020-03-18 21:22:39 +0200

    JavaScript form validation

---
+++ registration_form/registration_form.js
@@ -0,0 +1,48 @@
+function validUname(uname) {
+    let error = 0;
+    var length = uname.length;
+    var validChars = /\w{3,10}/;
+    document.getElementById('username_error').innerHTML = '';
+    if (length < 3 || length > 10 || !validChars.test(uname)) {
+        error++;
+        document.getElementById('username_error').innerHTML = "<em style='color:red'>Please enter valid username</em>";
+    }
+    return error;
+}
+function validPass(pass) {
+    let error = 0;
+    var length = pass.length;
+    var validChars = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
+    document.getElementById('password_error').innerHTML = '';
+    if (length < 6 || !validChars.test(pass)) {
+        error++;
+        document.getElementById('password_error').innerHTML = "<em style='color:red'>Please enter valid password</em>";
+    }
+    return error;
+}
+function validPassConfirm(passConfirm, pass) {
+    let error = 0;
+    document.getElementById('passwordConfirm_error').innerHTML = '';
+    if (pass.localeCompare(passConfirm) !== 0) {
+        error++;
+        document.getElementById('passwordConfirm_error').innerHTML = "<em style='color:red'>Passwords are different</em>";
+    }
+    return error;
+
+}
+function validate() {
+    let error = 0;
+    var uname = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    var passConfirm = document.getElementById("passwordConfirm").value;
+    error += validUname(uname);
+    error += validPass(pass);
+    error += validPassConfirm(pass, passConfirm);
+    if (error > 0) {
+
+        return false;
+    }
+    else {
+        return true;
+    }
+}
