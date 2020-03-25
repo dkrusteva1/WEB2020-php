9f7d22dcd726169358878f9de9d95ae5e14da323
dkrusteva1 <dessynceto@abv.bg>
2020-03-25 13:46:41 +0200

    Updated code

--- registration_form/registration_form.js
+++ registration_form/registration_form.js
@@ -12,9 +12,12 @@ function validUname(uname) {
 function validPass(pass) {
     let error = 0;
     var length = pass.length;
-    var validChars = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
+    let digits=/0-9/;
+    let sLetters=/a-z/;
+    let BLetters=/A-Z/;
+
     document.getElementById('password_error').innerHTML = '';
-    if (length < 6 || !validChars.test(pass)) {
+    if (length < 6 || !validChars.test(digits) || !validChars.test(sLetters) || !validChars.test(BLetters)) {
         error++;
         document.getElementById('password_error').innerHTML = "<em style='color:red'>Please enter valid password</em>";
     }
