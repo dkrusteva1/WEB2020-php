8e518b44e6ba81ca6a552c64b249910f09cbef96
dkrusteva1 <dessynceto@abv.bg>
2020-04-29 00:10:46 +0300

    Task 1 for adding electives in database

---
+++ db_task1/dbOperation.php
@@ -0,0 +1,20 @@
+<?php
+
+function connection()
+{
+    $host="localhost";
+    $db="www";
+    $user="root";
+    $password="";
+    $conn  = new PDO("mysql:host=$host; dbname=$db", $user, $password);
+          $courseTitle = $_POST['title'];
+          $name = $_POST['name'];
+          $description = $_POST['description'];
+          $stmt = $conn->prepare("INSERT INTO electives (title, description, lecturer) VALUES (:courseTitle, :description, :name);");
+          $stmt->bindParam(':courseTitle', $courseTitle);
+          $stmt->bindParam(':description', $description);
+          $stmt->bindParam(':name', $name);
+          $stmt->execute();
+}
+
+?>
